3334b729fcb2a831e04fddd3a6e72b1e74645975
Victor Ferreira da Silva <victor.ferreira@castgroup.com.br>
2024-10-17 17:21:41 -0400

    🏊🏻‍♂️ Expanded items working

--- app/components/settings/ExpansionBaseWrapper.vue
+++ app/components/settings/ExpansionBaseWrapper.vue
@@ -1,19 +1,29 @@
 <script setup lang="ts">
-const props = defineProps<{ label?: string }>()
-defineOptions({
-  inheritAttrs: false
-});
+const slots = defineSlots<{
+  properties: string;
+  decorators: string;
+  layout: string;
+  conditions: string;
+  attributes: string;
+  validation: string;
+  data: string;
+  options: string;
+}>()
 
 const { dark, localStorage } = useQuasar()
-const slots = useSlots()
+const formStore = useFormStore()
+const { setActiveField, copyField, removeField } = formStore
+
+const elementsClosed = localStorage.getItem('elements-closed')
+
+// const slots = useSlots()
 const slotsKeys = Object.keys(slots)
 
 // Retrieve and parse the localStorage item, and handle cases where it might be `null`
 // This variable is initialized only once
 const elementClosed: string[] = JSON.parse(localStorage.getItem('element-closed') || '[]')
-const isDefaultOpened = !!slotsKeys.length && !elementClosed.includes(slotsKeys.at(0) ?? '')
 
-// Define your sections
+// Allowed options 
 const sections: Record<string, string> = {
   properties: 'Propriedades',
   decorators: 'Decorações',
@@ -25,51 +35,111 @@ const sections: Record<string, string> = {
   options: 'Opções',
 }
 
+const label = ref('')
+const allExpanded = ref(!elementClosed.length || false)
+const expansionState = ref<Record<string, boolean>>({})
+
 // Compute the default label based on the available slots and sections
 const defaultLabel = computed(() => {
-  if (props.label) return props.label
+  if (label.value) return label.value
   const availableSlots = Object.keys(sections).filter((key) => slots[key])
   return availableSlots.length ? sections[availableSlots[0]!] : ''
 })
 
-function cachingDefaultClosed(isOpen: boolean) {
+// Dynamically get the available slots
+const availableSlots = computed(() => Object.keys(slots) as (keyof typeof slots)[])
+
+availableSlots.value.forEach(slotKey => {
+  expansionState.value[slotKey] = !elementClosed.includes(slotKey)
+})
+
+function cachingDefaultClosed(slotKey: keyof typeof slots, isOpen: boolean) {
   const elementClosed: string[] = JSON.parse(localStorage.getItem('element-closed') || '[]');
-  if (!slotsKeys.length || !Array.isArray(elementClosed)) return
+  if (!slotKey || !Array.isArray(elementClosed)) return
 
-  const slotKey = slotsKeys.at(0) ?? ''
-
-  if (!isOpen && slotKey) {
+  if (!isOpen) {
+    allExpanded.value = false
     // Only push to `elementClosed` if it's not already present
     if (!elementClosed.includes(slotKey)) {
       elementClosed.push(slotKey)
       localStorage.setItem('element-closed', JSON.stringify(elementClosed))
     }
-  } else if (isOpen && slotKey) {
+  } else if (isOpen) {
     const indexOf = elementClosed.findIndex(el => el === slotKey)
     if (indexOf !== -1) {
       elementClosed.splice(indexOf, 1)
       localStorage.setItem('element-closed', JSON.stringify(elementClosed))
     }
   }
+
+  expansionState.value[slotKey] = isOpen
+  if (Object.values(expansionState.value).every(Boolean)) {
+    allExpanded.value = true
+  }
+
+}
+
+function toggleExpandAll() {
+  allExpanded.value = !allExpanded.value
+
+  let elementClosed = JSON.parse(localStorage.getItem('element-closed') || '[]')
+
+  if (allExpanded.value) {
+    // Expand all
+    expansionState.value = availableSlots.value.reduce((acc, slotKey) => {
+      acc[slotKey] = true
+      return acc
+    }, {} as Record<string, boolean>)
+    elementClosed = []
+  } else {
+    // Collapse all
+    expansionState.value = availableSlots.value.reduce((acc, slotKey) => {
+      acc[slotKey] = false
+      return acc
+    }, {} as Record<string, boolean>)
+    elementClosed = availableSlots.value
+  }
+
+  localStorage.setItem('element-closed', JSON.stringify(elementClosed))
 }
 </script>
 
 <template>
-  <ClientOnly>
-    <q-expansion-item
-      :header-class="{ 'text-weight-semibold text-subtitle2': true, 'bg-grey-9 text-grey-11': dark.isActive, 'bg-blue-grey-1 text-blue-grey-10': !dark.isActive }"
-      :expand-icon-class="dark.isActive ? 'text-grey-5' : 'text-blue-grey-8'" :label="defaultLabel" v-bind="$attrs"
-      :default-opened="isDefaultOpened" @update:model-value="cachingDefaultClosed">
-      <template #default>
-        <slot name="properties" />
-        <slot name="decorators" />
-        <slot name="layout" />
-        <slot name="conditions" />
-        <slot name="attributes" />
-        <slot name="validation" />
-        <slot name="data" />
-        <slot name="options" />
-      </template>
-    </q-expansion-item>
-  </ClientOnly>
+  <q-list separator>
+    <q-item :class="{ 'bg-grey-9 text-grey-11': dark.isActive, 'bg-blue-grey-1 text-blue-grey-10': !dark.isActive }">
+      <q-item-section avatar>
+        <q-btn size="sm" flat dense round icon="close" :color="dark.isActive ? 'grey-5' : 'blue-grey-8'"
+          @click="setActiveField(null)" />
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label>
+          <h6 class="text-h6 no-margin">
+            {{ formStore.activeField?.name }}
+          </h6>
+        </q-item-label>
+      </q-item-section>
+
+      <q-item-section side>
+        <div class="q-gutter-xs">
+          <q-btn size="sm" flat dense round icon="o_content_copy" :color="dark.isActive ? 'grey-5' : 'blue-grey-8'"
+            @click="copyField(formStore.activeField)" />
+          <q-btn size="sm" flat dense round icon="o_delete" :color="dark.isActive ? 'grey-5' : 'blue-grey-8'"
+            @click="removeField(formStore.activeField)" />
+          <q-btn size="sm" flat dense round :icon="allExpanded ? 'minimize' : 'o_expand'"
+            :color="dark.isActive ? 'grey-5' : 'blue-grey-8'" @click="toggleExpandAll" />
+        </div>
+      </q-item-section>
+    </q-item>
+    <ClientOnly>
+      <q-expansion-item :model-value="expansionState[slotKey]"
+        :header-class="{ 'text-weight-semibold text-subtitle2': true, 'bg-grey-9 text-grey-11': dark.isActive, 'bg-blue-grey-1 text-blue-grey-10': !dark.isActive }"
+        :expand-icon-class="dark.isActive ? 'text-grey-5' : 'text-blue-grey-8'" :label="label || sections[slotKey]"
+        :default-opened="allExpanded ? true : !elementClosed.includes(slotKey)"
+        @update:model-value="val => cachingDefaultClosed(slotKey, val)" v-for="slotKey in availableSlots"
+        :key="slotKey">
+        <slot :name="slotKey" :foo="(newLabel: string) => { label = newLabel }" />
+      </q-expansion-item>
+    </ClientOnly>
+  </q-list>
 </template>
--- app/components/settings/QBtnConfig.vue
+++ app/components/settings/QBtnConfig.vue
@@ -41,88 +41,58 @@ function onEnteredProp(propName: string, propValue?: string) {
 }
 </script>
 <template>
-  <q-list separator>
-    <q-item :class="{ 'bg-grey-9 text-grey-11': dark.isActive, 'bg-blue-grey-1 text-blue-grey-10': !dark.isActive }">
-      <q-item-section avatar>
-        <q-btn size="sm" flat dense round icon="close" :color="dark.isActive ? 'grey-5' : 'blue-grey-8'"
-          @click="setActiveField(null)" />
-      </q-item-section>
-
-      <q-item-section>
-        <q-item-label>
-          <h6 class="text-h6 no-margin">
-            {{ formStore.activeField?.name }}
-          </h6>
-        </q-item-label>
-      </q-item-section>
-
-      <q-item-section side>
-        <div class="q-gutter-xs">
-          <q-btn size="sm" flat dense round icon="o_content_copy" :color="dark.isActive ? 'grey-5' : 'blue-grey-8'"
-            @click="copyField(formStore.activeField)" />
-          <q-btn size="sm" flat dense round icon="o_delete" :color="dark.isActive ? 'grey-5' : 'blue-grey-8'"
-            @click="removeField(formStore.activeField)" />
-          <q-btn size="sm" flat dense round icon="o_expand" :color="dark.isActive ? 'grey-5' : 'blue-grey-8'" />
-        </div>
-      </q-item-section>
-    </q-item>
-    <SettingsExpansionBaseWrapper>
-      <template #properties>
-        <q-card flat>
-          <q-card-section>
-            <div>
-              <div class="row align-center items-center justify-between">
-                <label for="form-name" @click="onClickLabel(propNameInputRef)">
-                  <span class="text-body2">
-                    Nome
-                  </span>
-                </label>
-                <q-input id="form-name" ref="propNameInputRef" v-model.trim="elementStates.name"
-                  :error="Boolean(elementStates.nameError)" :error-message="elementStates.nameError" hide-bottom-space
-                  filled color="cyan-8" dense type="text" @blur="onBlurName" />
-              </div>
+  <SettingsExpansionBaseWrapper>
+    <template #properties>
+      <q-card flat>
+        <q-card-section>
+          <div>
+            <div class="row align-center items-center justify-between">
+              <label for="form-name" @click="onClickLabel(propNameInputRef)">
+                <span class="text-body2">
+                  Nome
+                </span>
+              </label>
+              <q-input id="form-name" ref="propNameInputRef" v-model.trim="elementStates.name"
+                :error="Boolean(elementStates.nameError)" :error-message="elementStates.nameError" hide-bottom-space
+                filled color="cyan-8" dense type="text" @blur="onBlurName" />
             </div>
-          </q-card-section>
-          <q-separator :color="dark.isActive ? 'grey-9' : 'blue-grey-1'"/>
-          <q-card-section>
-            <div>
-              <div class="row align-center items-center justify-between">
-                <label for="form-label" @click="onClickLabel(propLabelInputRef)">
-                  <span class="text-body2">
-                    Rótulo
-                  </span>
-                </label>
-                <q-input id="form-label" ref="propLabelInputRef" v-model.trim="elementStates.label" hide-bottom-space
-                  filled color="cyan-8" dense type="text" @blur="onEnteredProp('label', elementStates.label)" />
-              </div>
+          </div>
+        </q-card-section>
+        <q-separator :color="dark.isActive ? 'grey-9' : 'blue-grey-1'" />
+        <q-card-section>
+          <div>
+            <div class="row align-center items-center justify-between">
+              <label for="form-label" @click="onClickLabel(propLabelInputRef)">
+                <span class="text-body2">
+                  Rótulo
+                </span>
+              </label>
+              <q-input id="form-label" ref="propLabelInputRef" v-model.trim="elementStates.label" hide-bottom-space
+                filled color="cyan-8" dense type="text" @blur="onEnteredProp('label', elementStates.label)" />
             </div>
-          </q-card-section>
+          </div>
+        </q-card-section>
 
-        </q-card>
-      </template>
-    </SettingsExpansionBaseWrapper>
-    <SettingsExpansionBaseWrapper label="Opções de botão">
-      <template #options>
-        <q-card flat>
-          <q-card-section>
-            <div>
-            </div>
-          </q-card-section>
-        </q-card>
-      </template>
-    </SettingsExpansionBaseWrapper>
-    <SettingsExpansionBaseWrapper>
-      <template #layout>
-        <q-card flat>
-          <q-card-section>
-            <div>
-            </div>
-          </q-card-section>
-        </q-card>
-      </template>
-    </SettingsExpansionBaseWrapper>
+      </q-card>
+    </template>
+    <template #options="{ foo }">
+      <q-card flat>
+        <q-card-section>
+          <div>
+          </div>
+        </q-card-section>
+      </q-card>
+    </template>
+    <template #layout>
+      <q-card flat>
+        <q-card-section>
+          <div>
+          </div>
+        </q-card-section>
+      </q-card>
+    </template>
+  </SettingsExpansionBaseWrapper>
 
-    <pre>{{ formStore.activeField }}</pre>
-  </q-list>
+  <pre>{{ formStore.activeField }}</pre>
 
 </template>
